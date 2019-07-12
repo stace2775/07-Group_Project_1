cfb4b0b1addba46e3076ef53c7683edd8121d6e7
Stacey <stacey.ellen.smith@gmail.com>
2019-07-11 20:53:57 -0500

    updated api keys file

--- api_keys.py
+++ api_keys.py
@@ -1,2 +1,4 @@
-# DATA.GOV API Key
-dg_key = "7OD5p7jAF2ktkNdz232bezpnha90B0d6He5RebsV"
+# DATA.GOV API Key 
+ses_dg_key = "7OD5p7jAF2ktkNdz232bezpnha90B0d6He5RebsV"
+km_dg_key="h6PvmzwIa7rXeB24Nrw1waxGedZEqqvOPdrU8MQb"
+ld_dg_key="Cv0UEdKdREoUTvZhV8jwZZhgUW8w9280oyRYcRmq"
